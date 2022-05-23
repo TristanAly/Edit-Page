ec2cc530b33b1c47e47a5f8aabe8d35ad7941fcc
AlyTristan <tristan.aly@gmail.com>
2022-05-23 10:47:16 +0200

    Initial Commit

---
+++ Edit Page/Edit_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edit_PageApp.swift
+//  Edit Page
+//
+//  Created by Tristan Aly on 23/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Edit_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
